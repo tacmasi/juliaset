a85f66ab5c4ff5c897b56823b4db846613ca4f1e
tacmasi <taka.massy@gmail.com>
2019-12-16 20:33:20 +0900

    update
    update

--- juliaset.py
+++ juliaset.py
@@ -1,17 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+filename = "a.png"
 def cabs(x, y):
     #return abs of x + iy
     return (x ** 2 + y ** 2)**(1/2)
 #######param
 #C(given)
-cx, cy = 0, 1
+#cx, cy = -1.4, -0.4
+cx, cy = +.01, -1
 R = (1 + (1 + 4 * cabs(cx, cy)**(1/2)))/2
 #set window
 minx, maxx = -2, 2
 miny, maxy = -2, 2
-num = 1000
+num = 480
 lx = np.linspace(minx, maxx, num)
 ly = np.linspace(miny, maxy, num)
 
@@ -31,16 +33,18 @@ def retf(x, y, cx, cy, R, maxitr,  n = 0):
     else:
         nx, ny = f(x, y, cx, cy)
         return retf(nx, ny, cx, cy, R, maxitr, n + 1)
-A = []
+A = [[]]*num
+i = 1
 for y in ly:
     tmp = []
     for x in lx:
         tmp.append(retf(x,y,cx,cy,R,10,n=0))
-    A.append(tmp)
+    A[-i] = tmp.copy()
+    i += 1
 #print(A)
 plt.figure()
 plt.imshow(A)
 plt.axis('off')
 plt.colorbar()
-#plt.show()
-plt.savefig("julia_i.png")
+plt.show()
+#plt.savefig(filename)
--- mandelbrotSet.py
+++ mandelbrotSet.py
@@ -6,16 +6,23 @@ def cabs(x, y):
     return (x ** 2 + y ** 2)**(1/2)
 #######param
 #filename
-filename = "mandelbrot.png"
+filename = "mandelbrot_5.png"
 #set window
-minx, maxx = -2, .75
-miny, maxy = -1.5, 1.5
-num = 1000
+#minx, maxx = -2, .75
+#miny, maxy = -1.5, 1.5
+pl = .03
+minx = -.77
+maxx = minx + pl
+miny = -.1 
+maxy = miny + pl 
+#minx, maxx = -1.9, -1.899
+#miny, maxy = -.00005, .00005
+num = 480
 lx = np.linspace(minx, maxx, num)
 ly = np.linspace(miny, maxy, num)
 
 #set maxitr
-maxitr = 20
+maxitr = 100
 ########
 #def
 def f(x, y, cx, cy):
@@ -30,12 +37,14 @@ def return_n(x, y, cx, cy, maxitr,  n = 0):
     else:
         nx, ny = f(x, y, cx, cy)
         return return_n(nx, ny, cx, cy, maxitr, n + 1)
-A = []
+A = [[]]*num
+i = 1
 for y in ly:
     tmp = []
     for x in lx:
         tmp.append(return_n(x,y,x,y,maxitr,n=0))
-    A.append(tmp)
+    A[-i] = tmp
+    i += 1
 #print(A)
 plt.figure()
 plt.imshow(A)
